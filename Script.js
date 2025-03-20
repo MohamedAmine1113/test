815bc0c06fd44c2b35c5b000363ba49c5a890f40
MohamedAmine1113 <mohamed.amine.bahmane@gmail.com>
2025-03-20 23:15:48 +0000

    erste push

---
+++ Script.js
@@ -0,0 +1,14 @@
+function personne (param1,param2) {
+    this.name = param1 ,
+    this.lastename = param2 
+}
+
+
+personne.prototype.getfillname = function() {
+    return this.name +' '+ this.lastename ;
+
+};
+
+const person = new personne("momo","bahmane")
+
+console.log(person.getfillname());
